7173a37e7aca32d69ac99c7fafd294141e25cc3c
Кирилл Лиховидов <cosmolihov3@gmail.com>
2024-10-13 22:29:06 +0300

    map drawing

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LinkThePort
 {
@@ -10,7 +11,58 @@ namespace LinkThePort
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+            char[,] map = ReadMap("world_map.txt");
+            DrawMap(map);
+        }
 
+        private static void DrawMap(char[,] map)
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                for (int x = 0; x < map.GetLength(0); x++)
+                {
+                    if (map[x, y] == 'o')
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                    else
+                        Console.BackgroundColor = defaultColor;
+
+                    Console.Write(map[x, y]);
+                    
+                    Console.BackgroundColor = defaultColor;
+                }
+                Console.Write("\n");
+            }
+        }
+
+        private static char[,] ReadMap(string path)
+        {
+            string[] file = File.ReadAllLines(path);
+
+            char[,] map = new char[GetMaxLengthOfLine(file), file.Length];
+
+            for (int x = 0; x < map.GetLength(0); x++)
+                for (int y = 0; y < map.GetLength(1); y++)
+                    map[x, y] = file[y][x];
+            
+            return map;
+        }
+
+        private static int GetMaxLengthOfLine(string[] lines)
+        {
+            int maxLength = lines[0].Length;
+
+            foreach (string line in lines)
+            {
+                if (line.Length > maxLength)
+                {
+                    maxLength = line.Length;
+                }
+            }
+
+            return maxLength;
         }
     }
 }
